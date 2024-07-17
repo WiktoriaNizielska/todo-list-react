9172c3f54fdd28a4dfb27c512601ba8564f179af
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-07-17 11:32:49 +0200

    text in AuthorPage modified and styled

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -1,6 +1,7 @@
 import Container from "../../common/Container";
 import Header from "../../common/Header";
 import Section from "../../common/Section";
+import { Wrapper } from "./styled";
 
 export default () => (
   <Container>
@@ -8,7 +9,9 @@ export default () => (
     <Section
       title="Wiktoria Nizielska"
       body={
-        <>nanana</>
+        <Wrapper>
+          Hej!👋 Jestem Wiktoria, mam 26 lat i jestem frontend developerem. Uwielbiam robić artystyczne rzeczy takie jak rysowanie czy malowanie. Z wykształcenia jestem kosmetologiem, więc estetyka i dokładność leżą w mojej naturze. Bardzo lubię organizację, tworzenie planów dnia i określanie celów. Sporo się nauczyłam pracując na produkcji i magazynach. Oprócz tego lubię spędzać czas spacerując po górach lub lasach. ⛰️🌲
+        </Wrapper>
       }
     />
   </Container>
---
+++ src/features/author/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.p`
+    line-height: 2;
+    padding: 0 20px;
+    text-align: justify;
+`;
